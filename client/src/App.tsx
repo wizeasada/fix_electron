6ffb840664a26915a65628e8ba4632767f30f7ce
wizeasada <60415861+wizeasada@users.noreply.github.com>
2021-02-13 16:35:58 +0900

    update

---
+++ client/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import AppRouter from './presentation/components/pages/AppRouter';
+
+//const store = configureStore({});
+
+const App: React.FC = () => (
+  <div className="App">
+    <AppRouter />
+  </div>
+);
+
+export default App;
---
+++ client/src/presentation/components/pages/AppRouter.tsx
@@ -0,0 +1,15 @@
+//TODO: AppRouterを正しい位置に変更
+import React from 'react';
+//import { addFnc } from '@core/shared';
+
+const AppRouter: React.FC = () => {
+  //addFnc(1, 2);
+  return (
+    <div>
+      <p>aaaa{/* <InputboxBox label="input" onChangeText={} /> */}</p>
+      <p>{/* <InputboxBox label="input" onChangeText={} /> */}</p>
+    </div>
+  );
+};
+
+export default AppRouter;
