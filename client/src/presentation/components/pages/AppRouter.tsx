0e17ee52a5929ed1e04fc55ed360b09536a12d21
wizeasada <60415861+wizeasada@users.noreply.github.com>
2021-02-16 12:00:23 +0900

    fix

--- client/src/presentation/components/pages/AppRouter.tsx
+++ client/src/presentation/components/pages/AppRouter.tsx
@@ -1,11 +1,11 @@
+import React, { useState } from 'react';
 //TODO: AppRouterを正しい位置に変更
-import React from 'react';
 //import { pullFunc } from 'shared/test';
 //const pull = pullFunc;
 //pull(2, 1);
 
 const AppRouter: React.FC = () => {
-  const [showModal, setShowModal] = React.useState(false);
+  const [showModal, setShowModal] = useState(false);
   return (
     <>
       <div className="m-5">
