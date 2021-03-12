3ab5c396705b3f2c2e9fa6cfb6b50c9b615f98fb
wizeasada <60415861+wizeasada@users.noreply.github.com>
2021-03-12 19:06:54 +0900

    update

--- client/src/presentation/mockups/stateType.tsx
+++ client/src/presentation/mockups/stateType.tsx
@@ -14,7 +14,6 @@ type TParticipatingMember = TMember & {
   userId: string;
   userName: string;
 };
-
 type TOfferingMember = TMember & {
   mail_address: string;
 };
@@ -23,10 +22,10 @@ type TElement = {
   id: string;
   name: string;
   link: string;
-  elements: (TElement[][]|TElement[]|[]);
+  elements: TElement[][] | TElement[] | [];
 };
 
-type TElementsBlock = (TElement[][]|TElement[]|[]);
+type TElementsBlock = TElement[][] | TElement[] | [];
 
 type TWorkSpace = {
   id: string;
@@ -39,50 +38,49 @@ type TWorkSpace = {
 export type TState = {
   loggedInUser: Omit<User, 'password'>; //ログイン中のユーザーの情報
   currentWorkSpaceId: string; //現在選択されているワークスペースのID
-  workSpaces: (TWorkSpace[]|[]); //全ワークスペース
+  workSpaces: TWorkSpace[] | []; //全ワークスペース
 };
 
 const mockLoggedInUserObj = {
   id: 'aaaaa',
   name: 'TaroYamada',
   mail_address: 'taro@gmail.com',
-} as const
+} as const;
 
 const mockCurrentWorkSpaceId = '11111111111';
 
 const mockMemberObj = {
-    id: '123456789',
-    isAdmin: false,
-    userId: '123456789',
-    userName: 'ワイズ'
-} as const
+  id: '123456789',
+  isAdmin: false,
+  userId: '123456789',
+  userName: 'ワイズ',
+} as const;
 
 export const state: TState = {
   loggedInUser: mockLoggedInUserObj,
   currentWorkSpaceId: mockCurrentWorkSpaceId,
-  workSpaces:[{
+  workSpaces: [
+    {
       id: '1',
       name: 'プロジェクト',
       iconFile: 'upload.png',
-      threeAggElementsBlocks:
-      [
+      threeAggElementsBlocks: [
         [
           {
             id: '2',
             name: 'SEEKS',
             link: 'https://nikkei2.com',
-            elements: 
-            [
+            elements: [
               { id: '3', name: '本番環境', link: 'https://nikkei3.com', elements: [] },
               { id: '4', name: '検証環境', link: 'https://nikkei4.com', elements: [] },
               { id: '5', name: 'ZenHub', link: 'https://nikkei5.com', elements: [] },
             ],
-          }
+          },
         ],
         [[]],
         [[]],
       ],
-      members :[mockMemberObj]
-    }
+      members: [mockMemberObj],
+    },
   ],
 };
