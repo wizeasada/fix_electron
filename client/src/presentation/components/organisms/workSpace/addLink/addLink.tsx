f6b97ae209f49cb3cca0e487a42a9acca7358987
wizeasada <60415861+wizeasada@users.noreply.github.com>
2021-04-09 12:42:03 +0900

    update

--- client/src/presentation/components/organisms/workSpace/addLink/addLink.tsx
+++ client/src/presentation/components/organisms/workSpace/addLink/addLink.tsx
@@ -23,6 +23,7 @@ const AddLink: React.FC = () => {
                               <label htmlFor="url" className="text-sm text-gray-600 dark:text-gray-400">
                                 タイトル
                                 たいとる１
+                                タイトル２
                               </label>
                             </div>
                             <input
